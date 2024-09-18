e8e831070ea78a814f9bc26c979b5cc43ed98cc8
cvalencia1991 <cesar4a6z@gmail.com>
2024-09-18 16:11:16 -0500

    Simplify the string of the desscription

--- src/store/modules/coaches/index.js
+++ src/store/modules/coaches/index.js
@@ -13,7 +13,8 @@ export default {
           lastName: 'Schwarzmüller',
           areas: ['frontend', 'backend', 'career'],
           description:
-          "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+          `I'm Maximilian and I've worked as a freelance web developer for years.
+           Let me help you become a developer as well!`,
           hourlyRate: 30
         },
         {
@@ -22,7 +23,8 @@ export default {
           lastName: 'Jones',
           areas: ['frontend', 'career'],
           description:
-          'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+          `I am Julie and as a senior developer in a big tech company, 
+          I can help you get your first job or progress in your current role.`,
           hourlyRate: 30
         }
       ]
