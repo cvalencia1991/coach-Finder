8205a275667037fc1150757d2657dcfb8ed14687
cvalencia1991 <cesar4a6z@gmail.com>
2024-09-19 15:20:52 -0500

    Update Desing of request recived message and email

--- src/pages/requests/RequestReceived.vue
+++ src/pages/requests/RequestReceived.vue
@@ -1,7 +1,53 @@
 <template>
   <section>
-    <h1>Request Received</h1>
-    <p>Your request has been successfully sent!</p>
-    <router-link to="/">Back to Home</router-link>
+    <base-card>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item 
+          v-for="req in receivedRequests" 
+          :key="req.id" 
+          :email="req.userEmail"
+          :message="req.message"/>
+      </ul>
+      <h3 v-else>You haven't received any requests yet.</h3>
+    </base-card>
   </section>
 </template>
+
+<script>
+import RequestItem from '../../components/requests/RequestItem.vue';
+
+export default {
+  components: {
+    RequestItem
+  },
+  computed: {
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+    receivedRequests() {
+      return this.$store.getters['requests/requests'];
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+
+</style>
