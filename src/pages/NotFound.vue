dd27a8db84e6e8bbb7427a9000f23bb9f5e26158
cvalencia1991 <cesar4a6z@gmail.com>
2024-09-20 10:05:22 -0500

    Adding Style for the Not Found Component

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,8 +1,8 @@
 <template>
   <section>
-    <h1>Page not found!</h1>
-  </section>
-  <section>
-    <router-link to="/">Back to Home</router-link>
+    <base-card>
+      <h2>Page not found!</h2>
+      <p>This page could not be found - maybe check out all our <router-link to="/coaches">coaches</router-link>?</p>
+    </base-card>
   </section>
 </template>
