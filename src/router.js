4694ccf5a5de586eedd7c35719477601107cc1a3
cvalencia1991 <cesar4a6z@gmail.com>
2024-09-21 14:23:35 -0500

    Implement navigation guards

--- src/router.js
+++ src/router.js
@@ -7,25 +7,52 @@ import ContactCoach from "./pages/requests/ContactCoach.vue";
 import RequestReceived from "./pages/requests/RequestReceived.vue";
 import NotFound from "./pages/NotFound.vue";
 import UserAuth from "./pages/auth/UserAuth.vue";
+import store from "./store/index.js";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", redirect: '/coaches' },
-    { path : '/coaches', component: CoachesList },
+    { path: "/", 
+      redirect: '/coaches' 
+    },
+    { path : '/coaches', 
+      component: CoachesList 
+    },
     { 
       path: "/coaches/:id", 
       props: true,
       component: CoachDetail, 
       children:[
-        { path: "contact", component: ContactCoach },
+        { path: "contact", 
+          component: ContactCoach //coaches/c1/contact
+        },
       ] 
     },
-    { path: "/register", component: CoachRegistration },
-    { path : "/requests", component: RequestReceived},
-    { path: "/auth", component: UserAuth },
+    { path: "/register", 
+      component: CoachRegistration, 
+      meta: { requiresAuth: true },
+    },
+    { path : "/requests", 
+      component: RequestReceived,
+      meta: { requiresAuth: true },
+    },
+    { path: "/auth", 
+      component: UserAuth,
+      meta: { requiresUnauth: true },
+    },
     { path : "/:notFound(.*)", component: NotFound}
   ]
 });
+
+// global navigation guard
+router.beforeEach((to, _, next) => {
+  if(to.meta.requiresAuth && !store.getters.isAuthenticated){
+    next('/auth');
+  } else if(to.meta.requiresUnauth && store.getters.isAuthenticated){
+    next('/coaches');
+  } else {
+    next();
+  } 
+})
 
 export default router
