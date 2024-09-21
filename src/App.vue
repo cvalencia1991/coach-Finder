88855c9a0171a91c0dac9d962b7ea82cf47ef2ea
cvalencia1991 <cesar4a6z@gmail.com>
2024-09-21 17:04:20 -0500

    Implement computed and watcher timer and logout active seeker

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,22 @@ export default {
   components: {
     TheHeader
   },
+  computed: {
+    didAutoLogout(){
+      return this.$store.getters.didAutoLogout;
+    }
+  },
   created() {
     this.$store.dispatch('autoLogin');
+  },
+  watch: {
+    didAutoLogout(curValue, oldValue){
+      if(curValue && curValue !== oldValue){
+        this.$router.replace('/coaches');
+      }
+    }
   }
+
 }
 </script>
 
