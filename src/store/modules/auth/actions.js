6be243cadc972c36756041578dec0bd54a31ba61
cvalencia1991 <cesar4a6z@gmail.com>
2024-09-21 17:02:28 -0500

    Set Auto LogOut

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,3 +1,4 @@
+let timer;
 export default {
   async login(context, payload) {
     return context.dispatch('auth', {
@@ -39,32 +40,58 @@ export default {
       throw error;
     }
 
+    const expiriesIn = +responseData.expiresIn * 1000;
+    const expirationDate = new Date().getTime() + expiriesIn;
+
     localStorage.setItem('token', responseData.idToken);
     localStorage.setItem('userId', responseData.localId);
+    localStorage.setItem('tokenExpiration', expirationDate);
+
+    timer = setTimeout(function(){
+      context.dispatch('autoLogout');
+    }, expiriesIn);
 
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
     })
   },
   autoLogin(context){
     const token = localStorage.getItem('token');
     const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+    const expiresIn = +tokenExpiration - new Date().getTime();
+
+    if(expiresIn < 0){
+      return;
+    }
+
+    setTimeout(function(){
+      context.dispatch('autoLogout');
+    }, expiresIn);
 
     if(token && userId){
       context.commit('setUser', {
         token: token,
         userId: userId,
-        tokenExpiration: null
       })
     }
   },
   logout(context){
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
+
+    clearTimeout(timer);
+
     context.commit('setUser', {
       token: null,
       userId: null,
-      tokenExpiration: null
     })
+  },
+  autoLogout(context){
+    context.dispatch('logout');
+    context.commit('setAutoLogout');
   }
 }
